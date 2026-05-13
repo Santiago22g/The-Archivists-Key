db1510b92bfd356d6762393ce9ec8d543d1e3a8f
santiago22 <tasiomartin33@gmail.com>
2026-05-13 13:05:23 +0200

    collisions added and mc modified movement animations

--- Scripts/main_character.gd
+++ Scripts/main_character.gd
@@ -1,16 +1,47 @@
 extends CharacterBody2D
 
-@export var speed: float = 300.0
+@export var speed: float = 250.0
+@onready var sprite = $AnimatedSprite2D
+
+var last_dir_string = "down" 
 
 func _physics_process(_delta: float) -> void:
-	# 1. Capturamos la dirección (configura estas teclas en Project Settings -> Input Map)
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
-	# 2. Aplicamos la velocidad
 	if direction != Vector2.ZERO:
 		velocity = direction * speed
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, speed)
-
-	# 3. Función mágica de Godot que maneja colisiones y deslizamiento
+	
 	move_and_slide()
+	
+	update_animation(direction)
+
+func update_animation(dir: Vector2):
+	var state = "idle"
+	
+	if dir != Vector2.ZERO:
+		state = "walk"
+		last_dir_string = get_direction_string(dir)
+	
+	var anim_name = last_dir_string + "-" + state
+	
+	if sprite.animation != anim_name:
+		sprite.play(anim_name)
+
+func get_direction_string(dir: Vector2) -> String:
+	var dir_name = ""
+	
+	if dir.y < -0.1: dir_name = "up"
+	elif dir.y > 0.1: dir_name = "down"
+	
+	var x_name = ""
+	if dir.x < -0.1: x_name = "left"
+	elif dir.x > 0.1: x_name = "right"
+	
+	if dir_name != "" and x_name != "":
+		return dir_name + "-" + x_name
+	elif dir_name != "":
+		return dir_name
+	else:
+		return x_name
