0a429a31c8c53887c6f072a85ab2aa44c0131005
aasujape-cmyk <aasujape@gmail.com>
2026-05-21 12:07:55 +0200

    new dialogues and fixed bugs

--- Scripts/Quijote/cyclops.gd
+++ Scripts/Quijote/cyclops.gd
@@ -1,14 +1,14 @@
 extends AnimatedSprite2D
 
 @export var resource: DialogueResource
-@export var title: String = "giant_shout"
+@export var title: String = "real_cyclops"
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("Entra aca")
 	if body.name == "MainCharacter":
-		var current_state = get_parent().mission_state
-		
-		if current_state == "read_note":
-			DialogueManager.show_example_dialogue_balloon(resource, title, [get_parent()])
-			
-			queue_free() 
+		body.set_physics_process(false)  
+		body.set_process(false)
+		body.update_animation(Vector2.ZERO)
+		DialogueManager.show_example_dialogue_balloon(resource, title, [self])
+		await DialogueManager.dialogue_ended
+		body.set_physics_process(true)  
+		body.set_process(true)
--- Scripts/Quijote/windmill.gd
+++ Scripts/Quijote/windmill.gd
@@ -4,13 +4,14 @@ extends Sprite2D
 @export var title: String = "giant_shout"
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("EntrA??")
 	if body.name == "MainCharacter":
+		body.set_physics_process(false)  
+		body.set_process(false)
+		body.update_animation(Vector2.ZERO)
 		var current_state = get_parent().mission_state
 		
 		if current_state == "read_note":
 			DialogueManager.show_example_dialogue_balloon(resource, title, [get_parent()])
-			
-
-		else:
-			get_tree().quit()
+			await DialogueManager.dialogue_ended
+		body.set_physics_process(true)  
+		body.set_process(true)
